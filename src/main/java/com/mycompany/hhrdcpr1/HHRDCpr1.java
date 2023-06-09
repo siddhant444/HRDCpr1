5cd9c0c7e34439ac1a614f8ada853d7b64fe1802
siddhant444 <135894142+siddhant444@users.noreply.github.com>
2023-06-09 23:59:24 +0530

    Add files via upload

---
+++ src/main/java/com/mycompany/hhrdcpr1/HHRDCpr1.java
@@ -0,0 +1,24 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.hhrdcpr1;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import javax.swing.JOptionPane;
+import login.login4;
+
+/**
+ *
+ * @author ASUS
+ */
+public class HHRDCpr1 {
+    
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+        new login4().setVisible(true);
+        
+    }
+}
